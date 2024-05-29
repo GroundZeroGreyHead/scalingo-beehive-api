e39bcc48b9c5afbc16ea92516d3f5de6373c47da
Mubarak basuudde <mubarak@guidhub.com>
2024-05-29 22:19:22 +0200

    create postgres user if not available

--- init.sql
+++ init.sql
@@ -1,2 +1,8 @@
-CREATE USER IF NOT EXISTS postgres;
-GRANT SUPERUSER TO postgres;
+DO $$
+BEGIN
+  IF NOT EXISTS (SELECT 1 FROM pg_catalog.pg_user WHERE usename = 'postgres') THEN
+    CREATE USER postgres;
+    ALTER USER postgres WITH SUPERUSER;
+  END IF;
+END;
+$$ LANGUAGE plpgsql;
